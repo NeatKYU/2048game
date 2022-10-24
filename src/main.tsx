454a6e89f1164aaf32f751bcef7bbf1864345f0f
jeff.park <jeff.park@machbase.com>
2022-10-24 14:38:20 +0900

    ➕ react-router-dom, recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 )
