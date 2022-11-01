101a473b4efd098dd41a16813622e4d3270227be
jeff.park <jeff.park@machbase.com>
2022-11-01 10:31:41 +0900

    ✨ 오른쪽 키 로직 완성

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -63,21 +63,38 @@ export const Board = (props: BoardProps) => {
                 tempBoard[i] = currentBoardRow;
 
                 for (let j = 1; j < 4; j++) {
-                    // if (tempBoard[i][j] === 0) {
-                    //     continue;
-                    // } else {
                     if (tempBoard[i][j] !== 0 && tempBoard[i][j] === tempBoard[i][j - 1]) {
                         // console.log('same ', i, j, i, j-1)
                         tempBoard[i][j - 1] = tempBoard[i][j] * 2;
                         tempBoard[i][j] = 0;
-                    } else {
-                        
                     }
-                    // }
                 }
             }
         } else if (moveKey === 'ArrowRight') {
-            
+            for (let i = 0; i < 4; i++) {
+                // ex) 0 64 32 0 => 0 0 64 32 / 0 2 0 2 => 0 0 2 2
+                const currentBoardRow = JSON.parse(JSON.stringify(tempBoard[i]));
+
+                for (let k = 0; k < 4; k++) {
+                    if (tempBoard[i][k] !== 0) {
+                        currentBoardRow.push(currentBoardRow[k])
+                    }
+                }
+                currentBoardRow.splice(0, 4)
+                while (currentBoardRow.length < 4) {
+                    currentBoardRow.unshift(0)
+                }
+                
+                tempBoard[i] = currentBoardRow;
+
+                for (let j = 2; j >= 0; j--) {
+                    if (tempBoard[i][j] !== 0 && tempBoard[i][j] === tempBoard[i][j + 1]) {
+                        // console.log('same ', i, j, i, j-1)
+                        tempBoard[i][j + 1] = tempBoard[i][j] * 2;
+                        tempBoard[i][j] = 0;
+                    }
+                }
+            }
         }
 
         // setBoard는 createNumber안에서 실행
@@ -88,9 +105,8 @@ export const Board = (props: BoardProps) => {
         // console.log('user press key = ', event.key)
         if (event.key === 'ArrowLeft') {
             moveNumber('ArrowLeft');
-            // console.log('current board = ', board, moveNumber('ArrowLeft'))
         } else if (event.key === 'ArrowRight') {
-            
+            moveNumber('ArrowRight')
         } else if (event.key === 'ArrowDown') {
     
         } else if (event.key === 'ArrowUp') {
